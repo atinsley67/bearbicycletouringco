52299ef66d494a4e390affe0c27b80c36b98a1de
Alex Tinsley <Alex Tinsley>
2016-06-18 11:45:58 -0400

    New social media badges, trip advisor widget

--- content/app/bottom.html.php
+++ content/app/bottom.html.php
@@ -1,31 +1,36 @@
     </div>
     <div class="footer">
       <div style="float: left;">
-        <br>
-        <br>
         <p><b>www.bearbicycletouring.com<b/></p>
         <p><b>&copy 2016 Bear Bicycle Touring Co.</b></p>
       </div>
       
-      
-      <div class="footerLinks" >
+      <div class="footerLinks">
 
-        <p><a href="http://www.facebook.com/bearbicycletouringco" target="_blank"><img src="<?php echo $rel_url?>static/images/facebook.png" alt="Bear Bicycle Touring on Facebook" /></a></p>
-        <p><a href="http://www.pinterest.com/bearbicycletour" target="_blank">
-          Get inspired on Pintrest
-          <img src="<?php echo $rel_url?>static/images/Pinterest-badge-20px.png" alt="Bear Bicycle Touring on Pintrest"/>
+        <a href="http://www.facebook.com/bearbicycletouringco" target="_blank">
+          <img src="<?php echo $rel_url?>static/images/facebook.png" alt="Bear Bicycle Touring on Facebook" />
         </a>
-        <p><a href="https://twitter.com/bear_bicycle_co" target="_blank">
-          Follow us on <span style="color: #55acee">Twitter</span>
+
+        <a href="https://www.instagram.com/bearbiketouring" target="_blank">
+          <img src="<?php echo $rel_url?>static/images/instagram.png" alt="Bear Bicycle Touring on Facebook" />
+        </a>
+        
+        <a href="http://www.pinterest.com/bearbicycletour" target="_blank">
+          <img src="<?php echo $rel_url?>static/images/pinterest.png" alt="Bear Bicycle Touring on Pintrest"/>
+        </a>
+
+        <a href="https://twitter.com/bear_bicycle_co" target="_blank">
           <img src="<?php echo $rel_url?>static/images/twitter.png" alt="Bear Bicycle Touring on Twitter"/>
         </a>
-        <p><style>.ig-b- { display: inline-block; float:right; margin-top:5px;}
-        .ig-b- img { visibility: hidden; }
-        .ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; }
-        .ig-b-v-24 { width: 137px; height: 24px; background: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24.png) no-repeat 0 0; }
-        @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) {
-        .ig-b-v-24 { background-image: url(//badges.instagram.com/static/images/ig-badge-view-sprite-24@2x.png); background-size: 160px 178px; } }</style>
-        <a href="https://www.instagram.com/bearbiketouring/?ref=badge" target="_blank" class="ig-b- ig-b-v-24"><img src="//badges.instagram.com/static/images/ig-badge-view-24.png" alt="Instagram" /></a>
+
+        <a href="https://plus.google.com/115830291435750562172/" target="_blank">
+          <img src="<?php echo $rel_url?>static/images/googleplus.png" alt="Bear Bicycle Touring on Twitter"/>
+        </a>
+
+        <a href="https://www.youtube.com/channel/UCoKWi5z66i-8J8b9d_-VXXA" target="_blank" >
+          <img src="<?php echo $rel_url?>static/images/youtube.png" alt="Bear Bicycle Touring on Instagram" />
+        </a>
+      
       </div>
       <p style="clear: both;">
     </div>
